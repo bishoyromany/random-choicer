e9aa8991105cfd967d5165c4fe2bd171e7a61f23
Bishoy Romany <7bishoyromany7@gmail.com>
2021-10-21 17:38:29 +0200

    Update Vercel Configs

--- src/components/global/Alert.vue
+++ src/components/global/Alert.vue
@@ -1,17 +1,16 @@
 <script setup>
-import {defineProps} from "vue";
+import { defineProps } from "vue";
 
 const props = defineProps({
-    classes: {
-        type: Array,
-        default: () => []
-    }
+  classes: {
+    type: Array,
+    default: () => [],
+  },
 });
-
 </script>
 
 <template>
-    <div class="w-full py-2 px-3 rounded-md" :class="props.classes">
-        <slot></slot>
-    </div>
+  <div class="w-full py-2 px-3 max-w-lg rounded-md" :class="props.classes">
+    <slot></slot>
+  </div>
 </template>
